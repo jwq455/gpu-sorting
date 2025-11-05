6b59974c5f7a82137215f4d1d92e7c9d658f8655
vdn588 <vdn588@hendrixfut03fl.unicph.domain>
2025-11-05 18:04:09 +0100

    last changes, script does not work after adding futhark

--- bench.sh
+++ bench.sh
@@ -4,10 +4,9 @@ tmp=tmp.txt
 benchs=benchs.txt
 
 rm "$benchs"
-#B=256 
 Qs=(21, 22, 23)
 lgHs=(7, 8, 9)
-Ns=(1000, 100000000, 1000000000)
+Ns=(100, 100000, 1000000000)
 Bs=(128, 256, 512)
 
 for B in ${Bs[@]}; do
@@ -21,11 +20,19 @@ for B in ${Bs[@]}; do
 				t_rad=$(grep "Radix Sorting" "$tmp"| sed -E 's/.*runs in: ([0-9.]+) us,.*/\1/')
 				k_cup=$(grep "CUB Sorting" "$tmp"| sed -E 's/.*Sorted keys per second: ([0-9.]+).*/\1/')
 				k_rad=$(grep "Radix Sorting" "$tmp" | sed -E 's/.*Sorted keys per second: ([0-9.]+).*/\1/')
+				fut_time=$(grep "data.in:" "$tmp" | sed -E 's/.*data.in: ([0-9.]+).*/\1/')
+				#valid=$(grep "INVALID" "$tmp")
+				echo "Run time Futhark: "$fut_time"" >> "$benchs"
+				echo "-------------------------------------------" >> "$benchs"
 				echo "Run time CUP sort: "$t_cup"" >> "$benchs"
-				echo "Nr of keys sorted pr second "$k_cup"" >> "$benchs"
+				echo "Nr of keys sorted pr second: "$k_cup"" >> "$benchs"
+				echo "-------------------------------------------" >> "$benchs"
 				echo "Run time Radix sort: " $t_rad"" >> "$benchs"
-				echo "Nr of keys sorted pr second "$k_rad"" >> "$benchs"
-				echo "-------------------------------------------" >> "$benchs" 
+				echo "Nr of keys sorted pr second: "$k_rad"" >> "$benchs"
+				#if [[! -n "$valid"]]; then
+   				#	 echo "INVLAID RESULT"
+				#fi 
+				echo "==========================================" >> "$benchs" 
 				rm "$tmp"
 			done
 		done
